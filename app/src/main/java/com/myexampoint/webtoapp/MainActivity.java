63ae8ab8902c8aa2352f08577fa71d899ef95c86
lockiey <159563769+lockiey@users.noreply.github.com>
2024-11-16 21:58:45 +0100

    Update MainActivity.java

--- app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
+++ app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     String ShowOrHideWebViewInitialUse = "show";
     private WebView webview ;
     private ProgressBar spinner;
-    String myurl = "https://rexana.vercel.app/"; //Change this  to your website hostname
+    String myurl = "mariam-241.vercel.app/calcule"; //Change this  to your website hostname
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
